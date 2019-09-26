dd671177319137e1eb17ee6b9bf6c8ef76ff6485
luisbarrera1 <labarrera@mail.csuchico.edu>
2019-09-25 17:20:38 -0700

    added the second part of the fortune

--- main.cpp
+++ main.cpp
@@ -11,22 +11,45 @@ int main()
 string firstName;
 string lastName;
 int lucky;
-
+int index= lastName.length()-1;
+char firstLetter;
+char lastLetter;
   //get user input
 
-cout<< "What is your first name? "<<endl;
+cout<<"What is your first name?"<<endl;
 cin>> firstName; 
 
-cout<< "What is your last name? "<<endl;
+cout<< "What is your last name?"<<endl;
 cin>> lastName;
 
 
-cout<< "wellcome, " <<firstName[0]<< "."<<lastName[0]<<". here is your fortune..."<<endl; 
+cout<< "Welcome, " <<firstName[0]<< "."<<lastName[0]<<"., here is your fortune..."<<endl; 
 
   //tell fortune
  
 
-cout<< "Your lucky number is " << firstName .length ()<< "."<<endl; 
+cout<< "your lucky number is " << firstName .length ()<< endl; 
+
+firstLetter=tolower(firstName[0]);
+
+
+if (firstLetter== 'a' || firstLetter== 'e' || firstLetter== 'i' || firstLetter == 'o' || firstLetter=='u')
+{
+        cout<< "you are destined to be famous."<<endl;
+}
+else
+{
+        cout<< "you should keep a low profile."<<endl;
+}
+
+lastLetter=tolower(lastName[index]);
+
+if(lastLetter== 'a' || lastLetter== 'e' || lastLetter== 'i' || lastLetter== 'o' || lastLetter== 'u')
+{
+        cout<<"you have already met your true love."<<endl;
+}
+
+cout<< "have a good day!"<<endl;
 
   return 0;
 }
